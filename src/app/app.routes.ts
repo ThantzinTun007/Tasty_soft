c216a5716efba0d16707e91715f1d0fef0ef896e
Thant Zin Tun <151204857+ThantzinTun007@users.noreply.github.com>
2026-01-13 15:24:39 +0630

    create folder structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { WelcomePage } from './pages/welcome-page/welcome-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: WelcomePage,
+  },
+];
